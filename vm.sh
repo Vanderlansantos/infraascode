36eb02d8fd89ff9d4f5eefc52ad02f514bab587b
vanderlan santos <vanderlan_go2010@hotmail.com>
2021-11-24 13:03:57 -0300

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,1 +1,1 @@
-az vm create --name srvUbuntu --resource-group Dev05 --image UbuntuLTS --authentication-type password --admin-username vanderlan --admin-password DerDer2727!@ --size Standard_B1S
+az vm criar --nome srvUbuntu --resource-group Dev05 --image UbuntuLTS - senha do tipo de autenticação --admin-username vanderlan --admin-password ******** --size Standard_B1S
